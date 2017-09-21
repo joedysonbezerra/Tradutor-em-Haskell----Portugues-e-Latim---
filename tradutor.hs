78612b9038a722bda75868c1a000efd3508c3765
Joédyson Bezerra <joedysonbezerra@gmail.com>
2017-09-21 17:10:05 -0300

    Adicionado comentário

--- tradutor.hs
+++ tradutor.hs
@@ -1,7 +1,7 @@
-junta :: String -> [String]
+junta :: String -> [String] --"eu tu ele" e transforma em ["eu","tu","ele"] quem faz é o words ele quer que a gente faça essa função words
 junta x = words x
 
-separa :: [String]->String
+separa :: [String]->String 
 separa [] = []
 separa (a:b) = tradutor a ++" "++ separa b
 
